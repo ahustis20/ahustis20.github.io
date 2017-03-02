271630e2ff5563e3a9bc8e781088a8be04c4608e
ahustis20 <ahustis20@gmail.com>
2017-03-02 08:41:52 -0700

    Update assessment3.js

--- html/assessment3.js
+++ html/assessment3.js
@@ -1,1 +1,13 @@
-
+ 
+  var guess = prompt("Please type in your password");
+  var password = "ilovekarl";
+        
+        if (guess==password) {
+          alert("You have been successfully logged in");
+      } else{
+          while (guess != password) {
+            guess = prompt("Incorrect. Please try again.");
+        }
+        alert("You have been successfully logged in");
+      }
+        
